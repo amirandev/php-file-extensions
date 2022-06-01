06d5afabf81b7c6dbbe84717c194eac41f312aaf
webtmdev <100407455+webtmdev@users.noreply.github.com>
2022-06-01 18:38:41 +0400

    upload files

---
+++ extensions.php
@@ -0,0 +1,21 @@
+<?php
+$extensions = [
+	'audio' => [
+		'mp3', 'weba', 'flac', 'midi', 'wav', 'm4a', 'wma', 'wave',
+		'aac', 'opus', 'm4p', 'mp2', 'amr', 'ogg', 'ac3', 'aif',
+		'aifc', 'aiff', 'au', 'caf', 'm4b', 'oga', 'sfark', 'voc'
+	],
+	'video' => [
+		'3g2', '3gp', '3gpp', 'avi', 'cavs', 'dv', 'dvr', 'flv',
+		'm2ts', 'm4v', 'mkv', 'mod', 'mov', 'mp4', 'mpeg', 'mpg',
+		'mts', 'mxf', 'ogg', 'rm', 'rmvb', 'swf', 'ts', 'vob',
+		'webm', 'wmv', 'wtv'
+	],
+	'image' => [
+		'3fr', 'arw', 'avif', 'bmp', 'cr2', 'cr3', 'crw', 'dcr',
+		'dng', 'eps', 'erf', 'gif', 'heic', 'icns', 'ico', 'jfif',
+		'jpeg', 'jpg', 'mos', 'mrw', 'nef', 'odd', 'odg', 'orf',
+		'pef', 'png', 'ppm', 'ps', 'psd', 'raf', 'raw', 'rw2', 
+		'tif', 'tiff', 'webp', 'x3f', 'xcf', 'xps'
+	]
+];  
